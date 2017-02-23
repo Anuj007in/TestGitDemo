712e4ffdfd98973621752a0a24577d3ebd39f125
Anuj007in <anuj_rov@yahoo.com>
2017-02-23 17:35:21 +0530

    New Add fun 34

--- DevOpsDemoPrj/src/HelloTest/TestDemo.java
+++ DevOpsDemoPrj/src/HelloTest/TestDemo.java
@@ -1,20 +1,9 @@
 package HelloTest;
 
-import java.util.Scanner;
-
 public class TestDemo {
 	public static void main(String[] args)
 	{
-	 
-		  int x, y, z;
-	      System.out.println("Enter two integers to calculate their sum ");
-	      //Scanner in = new Scanner(System.in);
-	    //  x = in.nextInt();
-	      //y = in.nextInt();
-	      x = 10;
-	      y = 20;
-	      z = x + y;;2
-	      System.out.println("Sum of entered integers = "+z);
+		System.out.print("Hi This is DevOps Demo--- CI- test 2 false2")
 		
 	}
 
