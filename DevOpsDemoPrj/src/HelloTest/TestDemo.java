f378bfabc57590f4580d0a76918af9e2130c33d8
Anuj007in <anuj_rov@yahoo.com>
2017-02-23 15:32:30 +0530

    New aks2 neg

--- DevOpsDemoPrj/src/HelloTest/TestDemo.java
+++ DevOpsDemoPrj/src/HelloTest/TestDemo.java
@@ -3,7 +3,7 @@ package HelloTest;
 public class TestDemo {
 	public static void main(String[] args)
 	{
-		System.out.print("Hi This is DevOps Demo--- CI- test 1");
+		System.out.print("Hi This is DevOps Demo--- CI- test 2 false")
 		
 	}
 
