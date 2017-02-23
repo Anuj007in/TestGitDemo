99bfdc4ae7146788ce5f3be1eea828725c63306e
Anuj007in <anuj_rov@yahoo.com>
2017-02-23 15:02:06 +0530

    New- issue

--- DevOpsDemoPrj/src/HelloTest/TestDemo.java
+++ DevOpsDemoPrj/src/HelloTest/TestDemo.java
@@ -1,7 +1,7 @@
 package HelloTest;
 
 public class TestDemo {
-	//public static void main(String[] args)
+	public static void main(String[] args)
 	{
 		System.out.print("Hi This is DevOps Demo--- CI test-1 with failure scenario")
 		
