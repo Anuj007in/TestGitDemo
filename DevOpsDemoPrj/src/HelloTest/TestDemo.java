764130b1ecbed8f1fbcfd605fe176be6ff25ff84
Anuj007in <anuj_rov@yahoo.com>
2017-02-23 18:45:26 +0530

    Fixed Bug 1231-new

--- DevOpsDemoPrj/src/HelloTest/TestDemo.java
+++ DevOpsDemoPrj/src/HelloTest/TestDemo.java
@@ -5,6 +5,25 @@ public class TestDemo {
 	{
 		System.out.print("Hi This is DevOps Demo--- CI- test 2 false2a");
 		
+		/*@Listeners(UtilitiesHelper.IListner.class)
+		public class TestCase_Login {
+			
+			WebDriver driver = null;
+			public TestCase_Login()
+			{
+				System.out.println("Hithis is constructor");
+				try {
+					//driver = ExcelUtility.getWebDriverfor("chrome");
+					driver = ExcelUtility.getWebDriverfor();
+					ExcelUtility.setExcelFile(UtilitiesHelper.Constants.Path_TestData, "Sheet1");
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.exit(1);
+					
+				}	
+			}*/
 	}
 
 }
